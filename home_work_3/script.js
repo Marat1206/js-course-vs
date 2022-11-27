dac51b53dfa8554144988e43bb10bea946227187
Marat <zhekovma@gmail.com>
2022-11-27 23:35:18 +0200

    hw_3

---
+++ home_work_3/script.js
@@ -0,0 +1,27 @@
+let firstQuestion = +prompt("Enter first number", "");
+let secondQuestion = +prompt("Enter second number", "");
+
+function add(firstQuestion, secondQuestion){
+  alert(firstQuestion + secondQuestion);
+}
+
+function sub(firstQuestion, secondQuestion){
+  alert(firstQuestion - secondQuestion);
+}
+
+function mult(firstQuestion, secondQuestion){
+  alert(firstQuestion * secondQuestion);
+}
+
+function div(firstQuestion, secondQuestion){
+  alert(firstQuestion / secondQuestion);
+}
+
+
+add(firstQuestion, secondQuestion);
+sub(firstQuestion, secondQuestion);
+mult(firstQuestion, secondQuestion);
+div(firstQuestion, secondQuestion);
+
+
+
