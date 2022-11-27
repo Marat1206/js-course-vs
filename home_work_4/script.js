96bf05631f2bea383a1cd3932136662916acd407
Marat <zhekovma@gmail.com>
2022-11-27 23:48:53 +0200

    hw_4

---
+++ home_work_4/script.js
@@ -0,0 +1,14 @@
+let actions = prompt("What action do you want to take? (add, sub, mult, div)").toLowerCase();
+
+let firstQuestion = +prompt("Enter first number");
+let secondQuestion = +prompt("Enter first number");
+
+if(actions == "add"){
+  alert(firstQuestion + secondQuestion);
+}else if(actions == "sub"){
+  alert(firstQuestion - secondQuestion);
+}else if(actions == "mult"){
+  alert(firstQuestion * secondQuestion);
+}else if(actions == "div"){
+  alert(firstQuestion / secondQuestion);
+}
