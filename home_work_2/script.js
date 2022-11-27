9b843bff8b3251e3360a1ddc68dd96ba1f7c4cd1
Marat <zhekovma@gmail.com>
2022-11-27 23:17:53 +0200

    hw_2

---
+++ home_work_2/script.js
@@ -0,0 +1,22 @@
+// task #1
+// let admin;
+// let name;
+ 
+// name = "Джон";
+
+// admin = name;
+
+// alert(admin);
+
+// ============
+
+// task #2
+
+// let planet = "Earth";
+// let currentUser;
+
+// let numToString = 10 + "10";
+
+// console.log(typeof(numToString));
+
+// ============
