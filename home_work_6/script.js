afcbcc072cb5284950df1885e929f2b118c82541
Marat <zhekovma@gmail.com>
2022-12-13 23:20:15 +0200

    h_w_6

---
+++ home_work_6/script.js
@@ -0,0 +1,17 @@
+Boolean(Number('10')) + 1; // 2
+
+//2
+'sub' + Number(false); // sub0
+
+//3
+16  *  '      91    ' // 1456
+
+let a = 16 * 91;
+
+console.log(a);
+
+//4
+true-70 // -69
+
+//5
+Number(1 + String(1)) + 1 // 12
