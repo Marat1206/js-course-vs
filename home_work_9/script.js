30f28799eaa2ab6eba9e783d4ec23f91c2af1659
Marat <zhekovma@gmail.com>
2022-12-15 00:14:04 +0200

    h_w_9 update

--- home_work_9/script.js
+++ home_work_9/script.js
@@ -58,3 +58,20 @@
 // =========================================
 
 
+// TASK#5
+
+// 5 > 4 // ture 
+
+// "ананас" > "яблоко" // false
+
+// "2" > "12" // true
+
+// undefined == null // true
+
+// undefined === null // false
+
+// null == "\n0\n" // false
+
+// null === +"\n0\n" // false
+
+
