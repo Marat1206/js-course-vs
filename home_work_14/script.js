0c415d800bf71142cfd65cb19132902cdfdf288e
Marat <zhekovma@gmail.com>
2022-12-24 20:51:30 +0200

    h_w_14

---
+++ home_work_14/script.js
@@ -0,0 +1,43 @@
+"use strict";
+// Task#1
+// for(let i = 1; i <= 100; i++){
+//   console.log(i);
+// }
+
+// Task#2
+// for(let i = 100; i >= 1; i--){
+//   console.log(i);
+// }
+
+// Task#3
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+
+//Task#4 
+// const arr = [1, 2, 3, 4, 5];
+
+// let arrSum = null;
+
+// for (let i = 0; i < arr.length; i++) {
+//     arrSum += arr[i];
+// }
+
+// console.log(arrSum);
+
+
+// Task#5
+// const arr = [1, 2, 3, 4, 5];
+
+// let arrSum = null;
+
+// for (let i = 0; i < arr.length; i++) {
+//     arrSum += Math.pow(arr[i], 2);
+// }
+
+// console.log(arrSum);
+
+
