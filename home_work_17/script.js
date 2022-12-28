c7d3f4e8d624014f9251e3d69462cebeec117b4a
Marat <zhekovma@gmail.com>
2022-12-28 23:08:38 +0200

    h_w_17

--- home_work_17/script.js
+++ home_work_17/script.js
@@ -1,20 +1,17 @@
 "use strict";
 
-let age = +prompt("Введите свой возвраст");
+let num = +prompt("Введите число");
 
-let text = "";
+let degree = +prompt("В какую степень возвести ваше число?");
 
-let count = age % 100;
-
-if (count == 1) {
-  text = "год";
-  if (count >= 5 && count <= 20) {
-    text = "лет";
+function exponentiation(num, degree = 1) {
+  if (!isNaN(num) && !isNaN(degree)) {
+    return Math.pow(num, degree);
   } else {
-    count = count % 10;
+    return "some error";
   }
-} else if (count >= 2 && count <= 4) {
-  text = "года";
-} else {
-  text = "лет";
 }
+
+let result = exponentiation(num, degree);
+
+alert(result);
