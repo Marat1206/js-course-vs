7b286d591d74b3aa97c3fbde152008ebae58cfe2
Marat <zhekovma@gmail.com>
2023-01-20 14:05:49 +0200

    h_w_20

---
+++ home_work_20/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const howMuchPotatoes = (borch = 48, potato = 75, priceOfOnePotato = 13) => {
+  const potatoesForLitr = potato * 4;
+
+  let numberOfPotatoes = (potatoesForLitr * borch) / 1000;
+
+  const potatoesTotalPrice = Math.ceil(numberOfPotatoes * priceOfOnePotato);
+
+  return `Количество Картошек на ${borch} литров борща: ${numberOfPotatoes} \n\n Цена за ${numberOfPotatoes} картошек составляет: ${potatoesTotalPrice} `;
+};
+
+console.log(howMuchPotatoes());
