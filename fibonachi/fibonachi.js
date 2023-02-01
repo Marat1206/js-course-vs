bafa175fb3e16813f9c2fb9e454f0cadbbae3f52
Marat <zhekovma@gmail.com>
2023-02-01 11:40:15 +0200

    update

--- fibonachi/fibonachi.js
+++ fibonachi/fibonachi.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const fib = (n = 1) => {
-  let a = 1;
+  let a = 0;
   let b = 1;
   for (let i = 3; i <= n; i++) {
     let c = a + b;
@@ -12,4 +12,4 @@ const fib = (n = 1) => {
   return b;
 };
 
-console.log(fib(12));
+console.log(fib(10));
