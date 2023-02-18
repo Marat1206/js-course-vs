a7d2b5a656f61679f3f519cc1ce89be0a5ea25ac
Marat <zhekovma@gmail.com>
2023-02-18 20:57:07 +0200

    h_w_26 optimized

--- home_work_26/script.js
+++ home_work_26/script.js
@@ -12,30 +12,19 @@ const factorial = (num) => {
 };
 
 
-// console.log(factorial(3));
+console.log(factorial(3));
 
 
 
 // Task 2
 
-const pow = (num, degree) => {
-  if (degree === 0) {
-    return 1;
-  } else {
-    return num * pow(num, degree - 1);
-  }
-};
+const pow = (num, degree) => degree === 0 ? 1 : num * pow(num, degree - 1);
+
+console.log(pow(2, 1));
 
 
-// console.log(pow(2, 3));
 
 // Task 3
-const sum = (a, b) => {
-  if (b === 0) {
-    return a;
-  } else {
-    return sum(a + 1, b - 1);
-  }
-};
+const sum = (a, b) => b === 0 ? a : sum(a + 1, b - 1);
 
 console.log(sum(10, 20));
